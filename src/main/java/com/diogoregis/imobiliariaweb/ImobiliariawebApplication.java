285ce36af54b703a8bcfd518385c42e37dab9a58
Diogo Regis <diogo.regis.silva@gmail.com>
2023-11-13 15:41:22 -0300

    commit inicial

---
+++ src/main/java/com/diogoregis/imobiliariaweb/ImobiliariawebApplication.java
@@ -0,0 +1,13 @@
+package com.diogoregis.imobiliariaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobiliariawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobiliariawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diogoregis/imobiliariaweb/ImobiliariawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diogoregis.imobiliariaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
