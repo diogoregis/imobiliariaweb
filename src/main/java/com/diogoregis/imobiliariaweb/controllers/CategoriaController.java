ef260d6126e8acebda7a0736789db7099d52b476
Diogo Regis <diogo.regis.silva@gmail.com>
2023-11-21 17:14:54 -0300

    criando e testando dto para categorias assim como pagina formulario

--- src/main/java/com/diogoregis/imobiliariaweb/controllers/CategoriaController.java
+++ src/main/java/com/diogoregis/imobiliariaweb/controllers/CategoriaController.java
@@ -1,15 +1,14 @@
 package com.diogoregis.imobiliariaweb.controllers;
 
 
+import com.diogoregis.imobiliariaweb.dto.CategoriaForm;
 import com.diogoregis.imobiliariaweb.models.Categoria;
 import com.diogoregis.imobiliariaweb.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -28,14 +27,29 @@ public class CategoriaController {
         return "listaCategorias";
     }
 
+    @GetMapping(value = "/form")
+    public String chamaFormulario(Model model){
+        //TODO
+        return "formCategoria";
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Categoria> findById(@PathVariable Long id){
         Categoria obj = categoriaRepository.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
+    @PostMapping(value = "/api")
     public void save(Categoria categoria){
         categoriaRepository.save(categoria);
+    }
+
+    @PostMapping
+    public String saveForm(@ModelAttribute CategoriaForm categoriaForm){
+        Categoria categoria = new Categoria();
+        categoria.setTipo(categoriaForm.getTipo());
+        categoriaRepository.save(categoria);
+        return "redirect:/categorias";
     }
 
 
---
+++ src/main/java/com/diogoregis/imobiliariaweb/dto/CategoriaForm.java
@@ -0,0 +1,10 @@
+package com.diogoregis.imobiliariaweb.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoriaForm {
+
+    private String tipo;
+
+}
